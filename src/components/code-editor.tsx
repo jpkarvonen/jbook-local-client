da7bcbe70a8c1c9ef1e5a607b2af9a498f348a24
John Karvonen <karvonen@Johns-MacBook-Pro.local>
2022-02-22 17:01:15 -0500

    install bulmaswatch and minor style update

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,3 +1,4 @@
+import './code-editor.css';
 import { useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
@@ -19,23 +20,29 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
   };
   const onFormatClick = () => {
-    //get current value
     const unformatted = editorRef.current.getModel().getValue();
-    //format value
-    const formatted = prettier.format(unformatted, {
-      parser: 'babel',
-      plugins: [parser],
-      useTabs: false,
-      semi: true,
-      singleQuote: true,
-    });
-    //set formatted value back in the editor
+
+    const formatted = prettier
+      .format(unformatted, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, '');
+
     editorRef.current.setValue(formatted);
     console.log(editorRef.current);
   };
   return (
-    <div>
-      <button onClick={onFormatClick}>Format</button>
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
