b7d5f5613158fbe104e46d37410e07a9042decd7
John Karvonen <karvonen@Johns-MacBook-Pro.local>
2022-02-24 19:19:08 -0500

    create action bar component

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -19,7 +19,7 @@ export const updateCell = (id: string, content: string): UpdateCellAction => {
   };
 };
 
-export const deleteeCell = (id: string): DeleteCellAction => {
+export const deleteCell = (id: string): DeleteCellAction => {
   return {
     type: ActionType.DELETE_CELL,
     payload: id,
